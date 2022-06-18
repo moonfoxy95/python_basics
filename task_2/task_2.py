5aee046022db1b1032976b9004107c497ae04a28
moonfoxy95 <102473676+moonfoxy95@users.noreply.github.com>
2022-06-18 19:46:28 +0300

    Add files via upload

---
+++ task_2/task_2.py
@@ -0,0 +1,10 @@
+"""
+2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
+выполнить подсчет количества строк, количества слов в каждой строке.
+"""
+
+
+with open('task_2.txt', 'r', encoding='utf-8') as f:
+    res = [f'{line}. {count.strip()} - {len(count.split())} words'
+           for line, count in enumerate(f, 1)]
+    print(*res, f'Strings - {len(res)}.', sep='\n')
